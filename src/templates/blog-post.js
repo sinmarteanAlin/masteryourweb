c22f45c89f79568a1bc58790399d27338cacd56b
Sinmartean Alin-Mircea <alin.sinmartean@visma.com>
2021-09-11 18:20:20 +0300

    ADDed disqus shortname

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,7 @@ import Share from "../components/share";
 export default class blogPost extends Component {
   render() {
     const data = this.props.data.contentfulBlogs;
-    const disqusShortname = "RohitGupta";
+    const disqusShortname = "https-www-masteryourweb-com";
     const disqusConfig = {
       identifier: data.id,
       title: data.title
